f0a4f3f6e29cf5d9c1c1cfcadb6a9f8fcd5951fe
Андрей Складчиков <skladchikov@company.dnevnik.ru>
2014-09-22 10:54:34 +0400

    Add basic tests

---
+++ test/js/comboboxSpec.js
@@ -0,0 +1,42 @@
+describe("combobox tests", function(){
+	var testUtils = React.addons.TestUtils;
+
+	it("Should export ReactComboBox to globals if no module system used", function(){
+		expect(window.ReactComboBox).toBeDefined();
+	});
+
+	it("Should set provided value to input", function(){
+		var testValue = "testValue";
+
+		var comboboxInstance = testUtils.renderIntoDocument(
+            ReactComboBox.ComboBox({options: [], value: testValue})
+        );
+
+        var inputValue = comboboxInstance.refs.textInput.getDOMNode().value;
+
+        expect(inputValue).toEqual(testValue);
+	});
+
+	it("Should open dropdown on input focus", function(){
+		var comboboxInstance = testUtils.renderIntoDocument(
+            ReactComboBox.ComboBox(null)
+        );
+
+        var inputNode = comboboxInstance.refs.textInput.getDOMNode();
+
+        testUtils.Simulate.focus(inputNode);
+
+        expect(comboboxInstance.state.isOpened).toBeTruthy();
+	});
+
+	it("Should render options into dropdown", function(){
+		var comboboxInstance = testUtils.renderIntoDocument(
+            ReactComboBox.ComboBox({options: ["test1", "test2"]})
+        );
+
+        var dropDown = comboboxInstance.getDOMNode().querySelector(".reactcombobox__dropdown");
+
+        expect(dropDown.innerHTML.indexOf("test1")!==-1).toBeTruthy();
+	});
+
+})
